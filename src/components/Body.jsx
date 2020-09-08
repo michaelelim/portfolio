8a35b8c97acc212b7c59c2b8447e3f857673d95f
Michael Lim <michaelelim@gmail.com>
2020-09-08 12:25:17 -0400

    Information about me added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,8 +4,14 @@ import './Body.scss'
 export default function Body(props) {
   return (
     <div className="body">
-      <h1 className="body__logo">{props.name}</h1>
-      <div><img className="body__hero-image" src={require("../assets/Images/Hero.jpg")} alt="Picture of Me"/></div>
+      <h1 className="body__title">{props.name}</h1>
+      <div className="body__wrapper">
+        <img className="body__hero-image" src={require("../assets/Images/Hero.jpg")} alt="Picture of Me"/>
+        <div className="body__hero-text">Good morning!  <br/><br/>
+          My name's Mike, and I'm a web and process developer.<br/>
+          I can make websites and apps, with a keen eye for process improvement. <br/><br/> 
+          I am looking for a company that values a team member that takes adversity (like a global pandemic) as an opportunity to follow a passion, take on new challenges, and start a new career path.</div>
+      </div>
     </div>
   )
 }
