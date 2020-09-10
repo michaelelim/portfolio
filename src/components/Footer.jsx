b900ab86dda76d31bd0af7e8594f7e4830ebe639
Michael Lim <michaelelim@gmail.com>
2020-09-09 23:51:25 -0400

    Added header options

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.scss'
 export default function Footer(props) {
   return (
     <div className="footer">
-      <h1 className="logo">{props.name}</h1>
+      <h1 className="footer__logo">{props.name}</h1>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,13 @@ import './Header.scss'
 export default function Header(props) {
   return (
     <div className="header">
-      <h1 className="logo">{props.name}</h1>
+      <h1 className="header__logo">{props.name}</h1>
+      <div className="header__rightwrapper">
+        <div className="header__option">Home</div>
+        <div className="header__option">About Me</div>
+        <div className="header__option">Projects</div>
+        <div className="header__option">Contact Me</div>
+      </div>
     </div>
   )
 }
