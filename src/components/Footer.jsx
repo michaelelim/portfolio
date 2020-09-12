4b0daed8ecb707bf83ac1b966605e6e82c9cffdb
Michael Lim <michaelelim@gmail.com>
2020-09-11 21:10:02 -0400

    Header underline and footer links added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ export default function Footer(props) {
     <div className="footer">
       <h1 className="footer__logo">{props.name}</h1>
       <div className="footer__iconwrapper">
-        <img className="footer__icons" src={require("../assets/Icons/SVG/logo-linkedin.svg")} alt="Linkedin logo"/>
-        <img className="footer__icons" src={require("../assets/Icons/SVG/mail-outline.svg")} alt="Email logo"/>
-        <img className="footer__icons" src={require("../assets/Icons/SVG/logo-github.svg")} alt="Github logo"/>
+        <a href="https://www.linkedin.com/in/michaelelim/" target="_blank"><img className="footer__icons" src={require("../assets/Icons/SVG/logo-linkedin.svg")} alt="Linkedin logo"/></a>
+        <a href="mailto:michaelelim@gmail.com" target="_blank"><img className="footer__icons" src={require("../assets/Icons/SVG/mail-outline.svg")} alt="Email logo"/></a>
+        <a href="https://github.com/michaelelim/" target="_blank"><img className="footer__icons" src={require("../assets/Icons/SVG/logo-github.svg")} alt="Github logo"/></a>
       </div>
     </div>
   )
