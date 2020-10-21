ca2e0a8566b98b021b6a6adddfabd9e64f52ec64
Michael Lim <michaelelim@gmail.com>
2020-10-21 10:37:04 -0400

    Added YDKDS to Portfolio and tweaks

--- src/components/BodyPortfolio.jsx
+++ src/components/BodyPortfolio.jsx
@@ -1,16 +1,48 @@
 import React from 'react'
+import ReactPlayer from "react-player"
 import './BodyPortfolio.scss'
 
 export default function BodyPortfolio(props) {
   return (
     <div className="portfolio">
       <h1 className="portfolio__title">{props.name}</h1>
+      <p className="portfolio__title">These are two examples of the projects I completed for the Web Dev course at BrainStation.</p>
+      
       <div className="portfolio__wrapper">
         <section className="portfolio__hero-text">
-          <p className="portfolio__hero-text-inner">These are two examples of the projects I completed for the Web Dev course at BrainStation.</p>
-          <h2 className="portfolio__hero-text-inner">24 Hour Hackathon presented by Bell</h2>
+          <h2 className="portfolio__hero-text-title">You Don't Know Diddly Squat</h2>
+          <a className="portfolio__hero-text-link" href="https://michaelelim-capstone-4p.netlify.app/" target="_blank" rel="noopener noreferrer">michaelelim-capstone-4p.netlify.app</a>
+
+          <div className="portfolio__hero-text-inner">You Don't Know Diddly Squat is an online multiplayer quiz game.</div>
+          <div className="portfolio__hero-text-inner">As a gamer, it was an enjoyable way to hone my skills.<br/>
+            As a parent, I wanted to have a game for the whole family.<br/>
+            Finally, I wanted to have a game that you could play during zoom calls.</div>
+          <div className="portfolio__hero-text-inner">Using the following tech stack:</div>
+          <ul className="portfolio__hero-text-inner">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>SASS</li>
+            <li>Javascript</li>
+            <li>React.JS</li>
+            <li>Node.JS</li>
+            <li>Express</li>
+            <li>Websockets</li>
+            <li>Socket.IO</li>
+            <li>Trivia API</li>
+            <li>Git</li>
+            <li>Netlify</li>
+            <li>Heroku</li>
+          </ul>        
+        </section>
+        
+        <ReactPlayer className="portfolio__hero-image" url="https://www.youtube.com/watch?v=trd6syJrSp0" width="30%"/>
+      </div>
+
+      <div className="portfolio__wrapper">
+        <section className="portfolio__hero-text">
+          <h2 className="portfolio__hero-text-title">24 Hour Bell Hackathon</h2>
           <div className="portfolio__hero-text-inner">The chosen topic was mental health, addiction and video games.</div>
-          <div className="portfolio__hero-text-inner">Using the following tech:</div>
+          <div className="portfolio__hero-text-inner">Using the following tech stack:</div>
           <ul className="portfolio__hero-text-inner">
             <li>HTML</li>
             <li>CSS</li>
@@ -20,7 +52,7 @@ export default function BodyPortfolio(props) {
             <li>Netlify</li>
           </ul>        
         </section>
-      <img className="portfolio__hero-image" src={require("../assets/Images/bellhackathon.png")} alt="Bell Hackathon"/>
+        <img className="portfolio__hero-image" src={require("../assets/Images/bellhackathon.png")} alt="Bell Hackathon"/>
       </div>
     </div>
   )
